a8137bb849828ddd22a1a18cefa350701f6f08cf
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-01-25 06:03:28 -0800

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,74 @@
+//=================================Importing all dependencies============
+var mongoose = require('mongoose'),
+    express  = require('express'),
+    router      = express.Router(),
+    mongoXlsx = require("mongo-xlsx"),
+    bodyParser = require("body-parser"),
+    multer    = require("multer"),
+    models  = require('../models/student'), //Importing multiple models and schemas
+    storage = multer.diskStorage(
+      {
+        destination: function (req, file, cb) {cb(null, 'uploads/')},
+        filename: function (req, file, cb) {cb(null, file.originalname)}
+      });
+
+var upload = multer({ storage: storage });
+//=======================================================================
+
+
+//======================================
+// GET Route
+//======================================
+router.get("/",function(req,res){
+  res.render("index.ejs")
+})
+
+
+
+
+//======================================
+// PoST Route
+//======================================
+router.post("/",upload.single("file-to-upload"),function (req,res){
+  console.log(req.file.path);
+  // var model = 32;
+  mongoXlsx.xlsx2MongoData("./"+req.file.path,models.studentSchema,function (err,mongoData) {
+    console.log("Mongo data:",mongoData);
+    //==========================for creating new objects and merging objects withsame Enrollment
+    mongoData.forEach(elem => {
+      models.Students.findById(elem._id,function (err,data) {
+        if (!data) {
+          console.log("creating and passing new obj");
+          var _id = elem._id,
+              nam = elem['name of mine']
+              // amount= elem.amount,
+              // age  = elem.age;
+          var obj = {_id:_id,name1:nam};
+          console.log("obj:",nam);
+          models.Students.create(obj);
+
+        } else {
+          console.log("found object - "+data);
+          var _id = elem._id,
+              name = elem.name,
+              amount= elem.amount,
+              age  = elem.age;
+          var obj = {_id:_id,name:name,age:age,Fees:{amount:amount}};
+          models.Students.findByIdAndUpdate(elem._id,obj,function(){
+            console.log("updated");
+
+          })
+        }
+      })
+    });
+    //==========================================
+  })
+  res.redirect("/")
+})
+
+
+
+
+
+
+module.exports = router;
